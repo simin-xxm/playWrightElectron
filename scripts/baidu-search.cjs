d5375478b15e18f7bbf1372698feec23a44b9759
Simin <59188897+simin-xxm@users.noreply.github.com>
2026-05-15 17:28:12 +0800

    Update baidu-search.cjs

--- scripts/baidu-search.cjs
+++ scripts/baidu-search.cjs
@@ -18,20 +18,12 @@ module.exports = {
     const executablePath = getChromiumPath();
     const browser = await chromium.launch({ headless: false, executablePath });
     const page = await browser.newPage();
-    await page.goto('https://h.kanglailab.com/login');
+    await page.goto('https://baidu.com');
 
-    await page.fill('#form_item_userName', 'lhq');
-    await page.fill('#form_item_password', '1234567');
+    await page.fill('#form_item_userName', '123');
+    await page.fill('#form_item_password', '321');
     await page.click('button:has-text("登 录")');
 
-    // 点击"设备管理"展开子菜单
-    await page.locator('[data-menu-id="/equip"] .ant-menu-title-content').click();
-    // 等待子菜单展开
-
-    await page.waitForTimeout(1000);
-    // 点击"打印机"
-    await page.locator('[data-menu-id="/equip/printerManagement"]').click();
-    await page.click('button:has-text("标本箱温度打印")');
 
     onLog('脚本执行完毕，等待手动关闭浏览器...');
     await page.waitForEvent('close', { timeout: 0 }).catch(() => {});
